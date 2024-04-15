0ed4e13944c7fc3c2e725b3977cfbfcb954c792c
romposdia <97699211+romposdia@users.noreply.github.com>
2024-04-15 17:12:12 +0200

    Add files via upload

---
+++ ansibleapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ansibleapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ansibleapi.settings')
+
+application = get_asgi_application()
